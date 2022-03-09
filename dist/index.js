3e9722312380d26831e0a7b074642fcda75e0a1e
nrandell <nick@nickrandell.com>
2022-03-09 11:42:40 +0000

    feat: reduce logging and upgrade versions

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var tslib_1 = require("tslib");
-// export * from "./createResolvers";
-(0, tslib_1.__exportStar)(require("./createSchemaCustomization"), exports);
-(0, tslib_1.__exportStar)(require("./onPluginInit"), exports);
-(0, tslib_1.__exportStar)(require("./pluginOptionsSchema"), exports);
-(0, tslib_1.__exportStar)(require("./sourceNodes"), exports);
